43e2e06deca8e89a5ee8dcad15f04c988c92ce49
Krithikkrsh <krshkrithik@gmail.com>
2022-01-26 14:24:49 +0530

    changes

--- src/main/java/com/oams/portal/PortalApplication.java
+++ src/main/java/com/oams/portal/PortalApplication.java
@@ -1,9 +1,14 @@
 package com.oams.portal;
 
+import com.oams.portal.config.FileStorageProperties;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class PortalApplication {
 
 	public static void main(String[] args) {
